7d625b3c973ccaf13acd28b99a4190d472e21540
Jumplez <vasilis.kikidis@gmail.com>
2024-06-22 20:39:03 +0300

    Bonus with AVL trees implemented

---
+++ Bonus/Search_Birth.h
@@ -0,0 +1,13 @@
+#ifndef SEARCH_BIRTH_H
+#define SEARCH_BIRTH_H
+
+#include "RBinarySearchTree.h"
+#include <iostream>
+#include <string>
+
+class Search_Birth {
+public:
+    void search(const RBinarySearchTree& bst);
+};
+
+#endif // SEARCH_BIRTH_H
